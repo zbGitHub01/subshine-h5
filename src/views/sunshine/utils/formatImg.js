948754eef5a572b7766394cb64e58785c939c707
zb.zhu <qqq980@163.com>
2022-11-28 18:13:44 +0800

    添加视频播放支持

--- src/views/sunshine/utils/formatImg.js
+++ src/views/sunshine/utils/formatImg.js
@@ -1,16 +1,25 @@
  
 // 根据盒子宽度按比例设置api图片的宽高
 export const computedImgAttribute = ( className, nextTick ) => {
-  var imgWidth = document.getElementsByClassName(className)[0].offsetWidth;
-  var imgArr = document.getElementsByTagName('img');
+  let boxWidth = document.getElementsByClassName(className)[0].offsetWidth;
+  let imgArr = document.getElementsByTagName('img');
+  let videoArr = document.getElementsByTagName('video');
   nextTick(()=>{
-    var imgList = Array.prototype.slice.call(imgArr);
-    for(var element in imgList) {
+    let imgList = Array.prototype.slice.call(imgArr);
+    let videoList = Array.prototype.slice.call(videoArr);
+    for(let element in imgList) {
       const curImgWidth = imgList[element].width;
       const curImgHeight = imgList[element].height;
-      const percentage = curImgWidth / imgWidth;
-      imgList[element].style.width = imgWidth + 'px';
+      const percentage = curImgWidth / boxWidth;
+      imgList[element].style.width = boxWidth + 'px';
       imgList[element].style.height = curImgHeight / percentage + 'px';
+    }
+    for(let element in videoList) {
+      const curVideoWidth = videoList[element].width;
+      const curVideoHeight = videoList[element].height;
+      const percentage = curVideoWidth / boxWidth;
+      videoList[element].style.width = boxWidth + 'px';
+      videoList[element].style.height = curVideoHeight / percentage + 'px';
     }
   })
 }
